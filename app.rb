f31ba5c2c53c591adc54f25746f645a1cf31a8e8
Sophie Gu <sophieqgu@gmail.com>
2019-08-20 05:13:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do 
     case params[:operation] 
     when "add"
-      "#{params[:number1] + params[:number2]}"
+      "#{params[:number1].to_i + params[:number2].to_i}"
     when "substract"
       "#{params[:number1]} - #{params[:number2]}"
     when "multiply"
