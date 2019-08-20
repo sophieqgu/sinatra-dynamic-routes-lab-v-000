041cd4621e4fee17b8a3450a8b50382357ee4d22
Sophie Gu <sophieqgu@gmail.com>
2019-08-20 05:18:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,8 +12,9 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do 
     params[:number].to_i.times do 
-      "#{params[:phrase]}"
+      @phrase << {params[:phrase]}
     end 
+    "#{@phrase.join("")"
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
